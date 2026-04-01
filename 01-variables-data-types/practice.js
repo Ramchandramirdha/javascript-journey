3dec3a04fe7b66cc52a3c8f4016d4780325bdf94
Ramchandramirdha <ramchandrakumarmirdha@zohomail.in>
2026-04-02 01:27:44 +0530

    Added practice examples for variables and data types

---
+++ 01-variables-data-types/practice.js
@@ -0,0 +1,14 @@
+// Practice - Variables & Data Types
+
+let name = "Ram";
+let age = 22;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// typeof examples
+console.log(typeof name);      // string
+console.log(typeof age);       // number
+console.log(typeof isStudent); // boolean
