56048c0b0c3003364d88f474cc975587857b1e81
Ramchandramirdha <ramchandrakumarmirdha@zohomail.in>
2026-04-02 01:40:27 +0530

    Added problems for variables and data types

---
+++ 01-variables-data-types/problems.js
@@ -0,0 +1,20 @@
+console.log(name, age, isStudent, empty, notDefined);
+
+// Problem 3: Convert string to number
+let num = "100";
+let converted = Number(num);
+
+console.log(converted); // 100
+console.log(typeof converted); // number
+
+// Problem 4: Boolean check
+let isLoggedIn = false;
+
+if (isLoggedIn) {
+  console.log("User logged in");
+} else {
+  console.log("User not logged in");
+}
+
+// Problem 5: typeof null (important concept)
+console.log(typeof null); // object (JavaScript bug)
